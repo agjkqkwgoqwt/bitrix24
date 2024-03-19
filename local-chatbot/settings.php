243138746265be0b4a2a5f371108bfbe2497b2df
agjkqkwgoqwt <kznexcbb5uduksyra0ca@gmail.com>
2024-03-19 16:52:50 +0300

    1

--- local-chatbot/settings.php
+++ local-chatbot/settings.php
@@ -1,6 +1,6 @@
 <?php
-//define('C_REST_CLIENT_ID','local.5c8bb1b0891cf2.87252039');//Application ID
-//define('C_REST_CLIENT_SECRET','SakeVG5mbRdcQet45UUrt6q72AMTo7fkwXSO7Y5LYFYNCRsA6f');//Application key
+define('C_REST_CLIENT_ID','local.65f99863b77ab9.17687286');//Application ID
+define('C_REST_CLIENT_SECRET','9n6rVwgR9aDDu4wXkQTfoxTKJ2nZdsbhhxJiXJIGA0xwceEI2t');//Application key
 // or
 //define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
 
